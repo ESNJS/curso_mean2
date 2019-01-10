c7dd3f348048c6a082485a40844cd2a4c26cde76
Erik <esnetto@gmail.com>
2019-01-10 08:31:53 -0300

    Seccion 4: Clase 21

---
+++ middlewares/authenticated.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'ClaveSecreta';
+
+exports.ensureAuth = function(req, res, next){
+  if(!req.headers.authorization){
+    return res.status(403).send({message: 'La peticion no tiene la cabezera de autenticacion'})
+  };
+
+  var token = req.headers.authorization.replace(/['"]+/g, '');
+
+  try{
+    var payload = jwt.decode(token, secret);
+
+    if(payload.exp <= moment().unix()){
+      return res.status(40).send({message: 'El Token ha expirado'})
+    };
+
+  }catch(ex){
+    console.log(ex);
+    return res.status(404).send({message: 'El Token no es valido'})
+  };
+
+  req.user = payload;
+
+  next();
+};
