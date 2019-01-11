80a6efd65be6774c08394967e8d91a2690d70317
ESNJS <nettoe@w7nettoer.redlink.com.ar>
2019-01-11 12:46:59 -0300

    Seccion 4: Clase 23

--- controllers/user.js
+++ controllers/user.js
@@ -98,9 +98,45 @@ function updateUser(req, res){
   });
 };
 
+function uploadImage(req, res) {
+  var userId = req.params.id;
+  var file_name = 'No subido...';
+
+  if(req.files){
+    var file_path = req.files.image.path;
+
+    var file_split = file_path.split('\\');
+    file_name = file_split[2];
+
+    var ext_split = file_name.split('\.');
+    var file_ext = ext_split[1];
+
+    if(file_ext == 'png'  || file_ext == 'gif' || file_ext == 'jpg'){
+      User.findByIdAndUpdate(userId, {image:  file_name}, (err, userUpdated) =>{
+        if(err){
+          res.status(500).send({message: 'Error al actualizar el usuario'});
+        }else{
+          if(!userUpdated){
+            res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+          }else{
+            res.status(200).send({user: userUpdated});
+          }
+        }
+      });
+    }else{
+      res.status(200).send({message: 'Extension del archivo no valida'});
+    };
+
+    console.log(file_split[2]);
+  }else{
+    res.status(200).send({message: 'La imagen no se ha subido'});
+  };
+};
+
 module.exports = {
   pruebas,
   saveUser,
   loginUser,
-  updateUser
+  updateUser,
+  uploadImage
 };
