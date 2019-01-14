68d0c892f16a87b316e854c46d4c7f8ea1dc1470
Erik <esnetto@gmail.com>
2019-01-14 08:33:19 -0300

    Seccion 5: Clase 25

---
+++ routes/artist.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var express = require('express');
+var ArtistController = require('../controllers/artist');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './uploads/artist'});
+
+api.get('/artist',md_auth.ensureAuth, ArtistController.getArtist);
+
+module.exports = api;
