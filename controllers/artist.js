d4580ebf45f5c12acb487b2a872cda85e2ce2b18
ESNJS <nettoe@w7nettoer.redlink.com.ar>
2019-01-14 18:07:35 -0300

    Seccion 5: Clase 29

--- controllers/artist.js
+++ controllers/artist.js
@@ -70,8 +70,26 @@ function saveArtist(req, res){
   });
 };
 
+function updateArtist (req, res){
+  var artistId = req.params.id;
+  var update = req.body;
+
+  Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated) => {
+    if(err){
+      res.status(500).send({message: "Error al actualizar el artista"});
+    }else{
+      if(!artistUpdated){
+        res.status(404).send({message: "Artista no actualizado"});
+      }else{
+        res.status(200).send({artist: artistUpdated});
+      };
+    };
+  });
+};
+
 module.exports = {
   getArtist,
   saveArtist,
-  getArtists
+  getArtists,
+  updateArtist
 };
--- routes/artist.js
+++ routes/artist.js
@@ -11,5 +11,6 @@ var md_upload = multipart({uploadDir: './uploads/artist'});
 api.get('/artist/:id',md_auth.ensureAuth, ArtistController.getArtist);
 api.post('/artist',md_auth.ensureAuth, ArtistController.saveArtist);
 api.get('/artists/:page?',md_auth.ensureAuth, ArtistController.getArtists);
+api.put('/artist/:id',md_auth.ensureAuth, ArtistController.updateArtist);
 
 module.exports = api;
