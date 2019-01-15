e0e3cf3450bb60d7ed6d980c5b6df3b0b3c02549
ESNJS <nettoe@w7nettoer.redlink.com.ar>
2019-01-15 18:30:43 -0300

    Seccion 7: Clase 45

--- controllers/album.js
+++ controllers/album.js
@@ -137,7 +137,7 @@ function uploadImage(req, res) {
           if(!albumUpdated){
             res.status(404).send({message: 'No se ha podido actualizar el album'});
           }else{
-            res.status(200).send({user: albumUpdated});
+            res.status(200).send({album: albumUpdated});
           }
         }
       });
--- controllers/song.js
+++ controllers/song.js
@@ -111,12 +111,57 @@ function deleteSong (req, res){
   });
 };
 
+function uploadFile(req, res) {
+  var songId = req.params.id;
+  var file_name = 'No subido...';
 
+  if(req.files){
+    var file_path = req.files.file.path;
+
+    var file_split = file_path.split('\\');
+    file_name = file_split[2];
+
+    var ext_split = file_name.split('\.');
+    var file_ext = ext_split[1];
+
+    if(file_ext == 'mp3'){
+      Song.findOneAndUpdate({_id: songId}, {file:  file_name}, (err, songUpdated) =>{
+        if(err){
+          res.status(500).send({message: 'Error al actualizar el album'});
+        }else{
+          if(!songUpdated){
+            res.status(404).send({message: 'No se ha podido actualizar el album'});
+          }else{
+            res.status(200).send({song: songUpdated});
+          }
+        }
+      });
+    }else{
+      res.status(200).send({message: 'Extension del archivo no valida'});
+    };
+  }else{
+    res.status(200).send({message: 'La imagen no se ha subido'});
+  };
+};
+
+function getSongFile(req,res){
+  var songFile = req.params.songFile;
+  var path_file = './uploads/songs/'+songFile;
+  fs.exists(path_file, function(exists){
+    if(exists){
+      res.sendFile(path.resolve(path_file));
+    }else{
+      res.status(200).send({message: 'La cancion no Existe'});
+    };
+  });
+};
 
 module.exports = {
   getSong,
   saveSong,
   getSongs,
   updateSong,
-  deleteSong
+  deleteSong,
+  uploadFile,
+  getSongFile
 };
