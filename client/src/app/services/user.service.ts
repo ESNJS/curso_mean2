542754656a99f6602d7ed658aa426ca404e84450
Erik <esnetto@gmail.com>
2019-01-17 18:07:34 -0300

    Seccion 10: Clase 59

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from './global';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class UserService{
@@ -13,8 +14,17 @@ export class UserService{
     this.url = GLOBAL.url;
   }
 
-  signup(){
-    return "hola mundo desde el servicio";
+  signup(user_to_login, gethash = null){
+    if(gethash != null){
+      user_to_login.gethash = gethash;
+    }
+    let json = JSON.stringify(user_to_login);
+    let params = json;
+
+    let headers = new Headers({'Content-Type':'application/json'});
+
+    return this._http.post(this.url+'login', params, {headers: headers})
+                     .map(res => res.json());
   }
 
 }
