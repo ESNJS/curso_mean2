b925dc3c0b8480a77237e3b8fa7f27c6e3657ca3
Erik <esnetto@gmail.com>
2019-01-22 21:00:14 -0300

    Seccion 10: Clase 64

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit{
   public identity;
   public token;
   public errorMessage;
+  public alertRegister;
 
   constructor(
     private _userService:UserService
@@ -97,8 +98,31 @@ export class AppComponent implements OnInit{
     this.token = null;
   };
 
+
   onSubmitRegister(){
     console.log(this.user_register);
+
+    this._userService.register(this.user_register).subscribe(
+      response => {
+        let user = response.user;
+        this.user_register = user;
+
+        if(!user._id){
+          this.alertRegister = 'error al registrarse';
+        }else{
+          this.alertRegister = 'El registro se ha relizado correctamente: ' + this.user_register.email;
+          this.user_register = new User('','','','','','ROLE_USER','');
+        }
+      },
+      error => {
+        var errorMessage = <any>error;
+
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.alertRegister = body.message;
+        }
+      }
+    );
   };
 
 }
