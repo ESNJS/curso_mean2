c7f56a81d3d17201544eed4abb1e6a19fdbd002f
Erik <esnetto@gmail.com>
2019-01-29 08:56:10 -0300

    Seccion 11: Clase 73

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
+import { GLOBAL } from '../services/global';
 
 @Component({
   selector: 'user-edit',
@@ -14,6 +15,7 @@ export class UserEditComponent implements OnInit{
   public identity;
   public token;
   public alertMessage;
+  public url: string;
 
   constructor(
     private _userService: UserService
@@ -24,6 +26,7 @@ export class UserEditComponent implements OnInit{
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.user = this.identity;
+    this.url = GLOBAL.url;
   };
 
   ngOnInit(){
@@ -38,9 +41,22 @@ export class UserEditComponent implements OnInit{
         if(!response.user){
           this.alertMessage = "El usuario no se ha actualizado";
         }else{
-          //this.user = response.user;
           localStorage.setItem('identity', JSON.stringify(this.user));
           document.getElementById('identity_name').innerHTML = this.user.name;
+
+          if(!this.filesToUpload){
+            //Redireccion
+          }else{
+            this.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload).then(
+              (result: any) => {
+                this.user.image = result.image;
+                localStorage.setItem('identity', JSON.stringify(this.user));
+
+              }
+            );
+          };
+          //this.user = response.user;
+
           this.alertMessage = "El usuario se ha actualizado";
         };
       },
@@ -56,6 +72,36 @@ export class UserEditComponent implements OnInit{
     );
   };
 
+  public filesToUpload: Array<File>;
 
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    console.log(this.filesToUpload);
+  };
+
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+    var token = this.token;
+    return new Promise(function(resolve, reject){
+      var formData:any = new FormData();
+      var xhr = new XMLHttpRequest();
+
+      for(var i = 0; i < files.length; i++){
+        formData.append('image', files[i], files[i].name);
+      };
+
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+          if(xhr.status == 200){
+            resolve(JSON.parse(xhr.response));
+          }else{
+            reject(xhr.response);
+          };
+        };
+      };
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader('Authorization', token);
+      xhr.send(formData);
+    });
+  };
 
 };
