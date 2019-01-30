fc7f4a66a01cba0fa2866f8399d1762c89680a44
Erik <esnetto@gmail.com>
2019-01-30 08:13:25 -0300

    Seccion 12: Clase 76

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,13 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 
-//import user
+//imports
 import { UserEditComponent } from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
 
 const appRoutes: Routes = [
-  {path: '', component: UserEditComponent},
-  {path: 'mis datos', component: UserEditComponent},
-  {path: '**', component: UserEditComponent}
+  {path: '', component: ArtistListComponent},
+  {path: 'artists/:page', component: ArtistListComponent},
+  {path: 'mis-datos', component: UserEditComponent},
+  {path: '**', component: ArtistListComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
