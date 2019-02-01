e2773b5c7d9da220808733bb73562a2cbed4cecf
Erik <esnetto@gmail.com>
2019-02-01 18:18:19 -0300

    Seccion 12: Clase 89

--- client/src/app/components/artist-list.component.ts
+++ client/src/app/components/artist-list.component.ts
@@ -3,11 +3,12 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { GLOBAL } from '../services/global';
 import { Artist } from '../models/artist';
+import { ArtistService } from '../services/artist.service';
 
 @Component({
   selector: 'artist-list',
   templateUrl: '../views/artist-list.html',
-  providers: [UserService]
+  providers: [UserService, ArtistService]
 })
 
 export class ArtistListComponent implements OnInit{
@@ -16,20 +17,57 @@ export class ArtistListComponent implements OnInit{
   public identity;
   public token;
   public url: string;
+  public next_page;
+  public prev_page;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
+    private _artistService: ArtistService,
     private _userService: UserService
   ){
     this.titulo = 'Artistas';
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.url = GLOBAL.url;
+    this.next_page = 1;
+    this.prev_page = 1;
   }
 
   ngOnInit(){
+    this.getArtists();
+  };
 
-  }
+  getArtists(){
+    this._route.params.forEach((params: Params) => {
+      let page = +params['page'];
+      if(!page){
+          page = 1;
+      }else{
+        this.next_page = page+1;
+        this.prev_page = page-1;
+
+        if(this.prev_page == 0){
+          this.prev_page = 1
+        }
+
+        this._artistService.getArtists(this.token, page).subscribe(
+          response => {
+            if(!response.artists){
+              this._router.navigate(['/']);
+            }else{
+              this.artists = response.artists;
+            }
+          },
+          error => {
+            var errorMessage = <any>error;
+            console.log(errorMessage);
+            if(errorMessage != null){
+              var body = JSON.parse(error._body);
+            }
+          }
+        )};
+      });//----forEach
+    };//----getArtists
 
 }
